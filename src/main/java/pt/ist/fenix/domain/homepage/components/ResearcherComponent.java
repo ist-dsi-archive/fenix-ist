27998f6abc2e0adeb481436089bea3eb3dac484c
Pedro Santos <pedro.san7os@gmail.com>
2015-01-07 17:22:29 +0000

    Fixed researcher sections of the homepage

--- src/main/java/pt/ist/fenix/domain/homepage/components/ResearcherComponent.java
+++ src/main/java/pt/ist/fenix/domain/homepage/components/ResearcherComponent.java
@@ -7,6 +7,9 @@ import org.fenixedu.cms.domain.component.DynamicComponent;
 import org.fenixedu.cms.rendering.TemplateContext;
 import org.fenixedu.commons.i18n.I18N;
 
+import pt.ist.fenix.FenixIstConfiguration;
+import pt.ist.fenix.domain.homepage.HomepageSite;
+
 @ComponentType(name = "Researcher Section Data Component",
         description = "Provides homepage owner's researcher section page data.")
 public class ResearcherComponent extends ResearcherComponent_Base {
@@ -26,8 +29,8 @@ public class ResearcherComponent extends ResearcherComponent_Base {
     @Override
     public void handle(Page page, TemplateContext local, TemplateContext global) {
         global.put("bundle", getTitleBundle());
-        global.put("researcher", page.getSite().getCreatedBy().getUsername());
-        global.put("sotisUrl", "https://sotis.tecnico.ulisboa.pt"); //FIXME get real configuration property when available
+        global.put("researcher", ((HomepageSite) page.getSite()).getOwner().getUsername());
+        global.put("sotisUrl", FenixIstConfiguration.getConfiguration().sotisURL());
         global.put("language", I18N.getLocale().toLanguageTag());
         global.put("dataKey", getDataKey());
         global.put("titleKey", getTitleKey());
