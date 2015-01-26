0f7ca58d91d74fd6cb0eef3c1b3554ae334cae6d
Artur Ventura <artur.ventura@gmail.com>
2015-01-26 21:18:14 +0000

    Adding Shim to old news sync service

---
+++ src/main/java/pt/ist/fenix/api/OldNewsSyncService.java
@@ -0,0 +1,93 @@
+package pt.ist.fenix.api;
+
+import org.fenixedu.bennu.core.rest.BennuRestResource;
+import org.fenixedu.cms.domain.Post;
+import org.fenixedu.cms.domain.Site;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.springframework.web.util.HtmlUtils;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+// DO NOT CHANGE THIS CLASS EVER AGAIN.
+
+@Path("/fenix-ist/oldNewsSyncService")
+public class OldNewsSyncService extends BennuRestResource{
+    private static final String EVENTS = "678604861135";
+    private static final String NEWS = "678604861136";
+
+    final static Locale PT = new Locale("pt");
+    final static Locale EN = new Locale("en");
+
+    @GET
+    @Produces("application/xml")
+    @Path("/")
+    public Response get(@QueryParam("announcementBoardId") String announcementBoardId, @QueryParam("selectedYear") int selectedYear, @QueryParam("selectedMonth") int selectedMonth,
+            @QueryParam("language") String language) {
+        Set<Post> posts;
+        if (announcementBoardId.equals(EVENTS)) {
+            posts = Site.fromSlug("tecnicolisboa").categoryForSlug("eventos").getPostsSet();
+        } else {
+            posts = Site.fromSlug("tecnicolisboa").categoryForSlug("noticias").getPostsSet();
+        }
+        Locale locale;
+        if (language == "pt") {
+            locale = PT;
+        } else {
+            locale = EN;
+        }
+
+        DateTime begin =
+                new DateTime().toDateMidnight().withYear(selectedYear).withMonthOfYear(selectedMonth).withDayOfMonth(1)
+                        .toDateTime();
+        DateTime end =
+                begin.dayOfMonth().withMaximumValue().hourOfDay().withMaximumValue().minuteOfHour().withMaximumValue()
+                        .secondOfMinute().withMaximumValue();
+        Interval i = new Interval(begin, end);
+
+        String result = "SUCCESS\n";
+        result += "<list>\n";
+        int index = 1;
+        for (Post post : posts.stream().sorted(Post.CREATION_DATE_COMPARATOR).filter(x -> i.contains(x.getPublicationBegin()))
+                .collect(Collectors.toList())) {
+            result += "  <net.sourceforge.fenixedu.presentationTier.Action.externalServices.AnnouncementDTO>\n";
+            result += "    <creationDate>" + post.getCreationDate().toString("dd/MM/yyyy HH:mm:ss") + "</creationDate>\n";
+            result +=
+                    "    <referedSubjectBegin>" + post.getPublicationBegin().toString("dd/MM/yyyy HH:mm:ss")
+                            + "</referedSubjectBegin>\n";
+            result +=
+                    "    <publicationBegin>" + post.getPublicationBegin().toString("dd/MM/yyyy HH:mm:ss") + "</publicationBegin>\n";
+            result += "    <publicationEnd>" + post.getPublicationEnd().toString("dd/MM/yyyy HH:mm:ss") + "</publicationEnd>\n";
+            result +=
+                    "    <lastModification>" + post.getModificationDate().toString("dd/MM/yyyy HH:mm:ss") + "</lastModification>\n";
+
+            result += "    <subject>" + post.getName().getContent(locale) + "</subject>\n";
+            result += "    <keywords></keywords>\n";
+            result += "    <body>" + HtmlUtils.htmlEscape(post.getBody().getContent(locale)) + "</body>\n";
+            result += "    <author>GCRP</author>\n";
+            result += "    <authorEmail>gcrp@ist.utl.pt</authorEmail>\n";
+            result += "    <place></place>";
+            result += "    <visible>" + post.isVisible() + "</visible>\n";
+            result += "    <visible>" + post.getExternalId() + "</visible>\n";
+            result += "    <photoUrl></photoUrl>\n";
+            result += "    <campus>Alameda</campus>\n";
+            result += "    <categories/>\n";
+            result += "    <pressRelease>false</pressRelease>\n";
+            result += "    <sticky>" + false + "</visible>\n";
+            result += "    <priority>" + index++ + "</priority>\n";
+            result += "  </net.sourceforge.fenixedu.presentationTier.Action.externalServices.AnnouncementDTO>\n";
+        }
+
+        result += "</list>";
+        return Response.ok(result).build();
+
+    }
+}
+
