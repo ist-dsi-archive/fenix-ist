00752c993587d066389abe8008998120d60cef98
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2015-01-27 11:06:27 +0000

    Added 410 status codes for several discontinued paths

---
+++ src/main/java/pt/ist/fenix/ui/struts/action/GoneActions.java
@@ -0,0 +1,41 @@
+package pt.ist.fenix.ui.struts.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.fenixedu.bennu.struts.annotations.Mapping;
+
+public class GoneActions extends Action {
+
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.sendError(410, "Gone for good");
+        return null;
+    }
+
+    @Mapping(path = "/executionCourse", module = "publico")
+    public static class OldExecutionCourseDA extends GoneActions {
+    }
+
+    @Mapping(path = "/announcementManagement", module = "publico")
+    public static class OldAnnouncementManagementDA extends GoneActions {
+    }
+
+    @Mapping(path = "/showDegreeTheses", module = "publico")
+    public static class OldShowDegreeThesisDA extends GoneActions {
+    }
+
+    @Mapping(path = "/showDegreeSite", module = "publico")
+    public static class OldShowDegreeSiteDA extends GoneActions {
+    }
+
+    @Mapping(path = "/department/theses", module = "publico")
+    public static class OldDepartmentThesisDA extends GoneActions {
+    }
+
+}
