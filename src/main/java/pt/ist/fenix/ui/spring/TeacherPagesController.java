deee54d9c3c199f1e995c51ddd686bf3383193ee
Miguel Borges <mb.borgez@gmail.com>
2015-01-27 18:52:28 +0000

    import content from previous execution course sites of a given execution course

--- src/main/java/pt/ist/fenix/ui/spring/TeacherPagesController.java
+++ src/main/java/pt/ist/fenix/ui/spring/TeacherPagesController.java
@@ -18,13 +18,14 @@
  */
 package pt.ist.fenix.ui.spring;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.fenixedu.academic.domain.ExecutionCourse;
 import org.fenixedu.academic.domain.Professorship;
-import org.fenixedu.academic.predicate.AccessControl;
 import org.fenixedu.academic.ui.spring.controller.teacher.ExecutionCourseController;
 import org.fenixedu.academic.ui.struts.action.teacher.ManageExecutionCourseDA;
+import org.fenixedu.cms.domain.Menu;
+import org.fenixedu.cms.domain.MenuItem;
+import org.fenixedu.commons.i18n.LocalizedString;
+import org.fenixedu.learning.domain.executionCourse.ExecutionCourseSite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,10 +35,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.RedirectView;
-import pt.ist.fenixframework.FenixFramework;
+import pt.ist.fenixframework.Atomic;
 
-import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+import java.util.stream.Stream;
 
+import static java.util.function.Predicate.isEqual;
+import static java.util.stream.Collectors.toList;
+import static org.fenixedu.academic.predicate.AccessControl.check;
+import static org.fenixedu.academic.predicate.AccessControl.getPerson;
 import static pt.ist.fenixframework.FenixFramework.atomic;
 
 @Controller
@@ -49,21 +56,59 @@ public class TeacherPagesController extends ExecutionCourseController {
 
     @RequestMapping(method = RequestMethod.GET)
     public TeacherPagesView all(Model model, @PathVariable ExecutionCourse executionCourse) {
-        Professorship professorship = executionCourse.getProfessorship(AccessControl.getPerson());
-        AccessControl.check(person -> professorship != null && professorship.getPermissions().getSections());
+        hasAccess(executionCourse);
         model.addAttribute("executionCourse", executionCourse);
-        model.addAttribute("professorship", professorship);
+        model.addAttribute("professorship", executionCourse.getProfessorship(getPerson()));
         model.addAttribute("site", executionCourse.getSite());
+        model.addAttribute("previousExecutionCourses", previousExecutionCourses(executionCourse).collect(toList()));
         return new TeacherPagesView();
     }
 
     @RequestMapping(value = "options", method = RequestMethod.POST)
     public RedirectView editOptions(@PathVariable ExecutionCourse executionCourse,
             @RequestParam(required = false, defaultValue = "") String alternativeSite) {
-        Professorship professorship = executionCourse.getProfessorship(AccessControl.getPerson());
-        AccessControl.check(person -> professorship != null && professorship.getPermissions().getSections());
+        hasAccess(executionCourse);
         atomic(() -> executionCourse.getSite().setAlternativeSite(alternativeSite));
         return new RedirectView(String.format("/teacher/%s/pages", executionCourse.getExternalId()), true);
+    }
+
+    @RequestMapping(value = "copyContent", method = RequestMethod.POST)
+    public RedirectView copyContent(@PathVariable ExecutionCourse executionCourse,
+            @RequestParam ExecutionCourse previousExecutionCourse) {
+        canCopyContent(executionCourse, previousExecutionCourse);
+        copyContent(previousExecutionCourse.getSite(), executionCourse.getSite());
+        return new RedirectView(String.format("/teacher/%s/pages", executionCourse.getExternalId()), true);
+    }
+
+    @Atomic
+    private void copyContent(ExecutionCourseSite from, ExecutionCourseSite to) {
+        Menu newMenu = to.getMenusSet().stream().findAny().get();
+        LocalizedString newPageName = new LocalizedString().with(Locale.getDefault(),
+                from.getExecutionCourse().getExecutionPeriod().getQualifiedName());
+        MenuItem emptyPageParent = service.create(to, null, newPageName, new LocalizedString()).get();
+        emptyPageParent.getPage().setPublished(false);
+        emptyPageParent.setTop(newMenu);
+        for(Menu oldMenu : from.getMenusSet()) {
+            oldMenu.getItemsSet().stream().forEach(menuItem -> service.copyStaticPage(menuItem, to, newMenu, emptyPageParent));
+        }
+    }
+
+    private Stream<ExecutionCourse> previousExecutionCourses(ExecutionCourse executionCourse) {
+        return executionCourse.getAssociatedCurricularCoursesSet().stream()
+                .flatMap(curricularCourse -> curricularCourse.getAssociatedExecutionCoursesSet().stream())
+                .filter(ec -> ec.getAcademicInterval().isBeforeNow())
+                .filter(ec -> ec.getSite() != null && ec.getSite().getPublished())
+                .sorted(ExecutionCourse.EXECUTION_COURSE_COMPARATOR_BY_EXECUTION_PERIOD_AND_NAME.reversed());
+    }
+
+    private void canCopyContent(ExecutionCourse executionCourse, ExecutionCourse previousExecutionCourse) {
+        hasAccess(executionCourse);
+        check(p -> previousExecutionCourses(executionCourse).filter(isEqual(previousExecutionCourse)).findAny().isPresent());
+    }
+
+    private void hasAccess(ExecutionCourse executionCourse) {
+        Professorship professorship = executionCourse.getProfessorship(getPerson());
+        check(person -> professorship != null && professorship.getPermissions().getSections());
     }
 
     @Override
